d3220ce9ca25b14bf0dabc22090d43053de1d7e2
amarcel <postbenno@gmail.com>
2022-05-09 21:07:54 -0400

    first commit

---
+++ src/main/java/com/wells/speech/SpeechApplication.java
@@ -0,0 +1,22 @@
+package com.wells.speech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@RestController
+public class SpeechApplication {
+
+	@GetMapping("/message")
+	public String message(){
+		return "Congrats ! your application deployed successfully in Azure Platform. !";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeechApplication.class, args);
+	}
+
+}
